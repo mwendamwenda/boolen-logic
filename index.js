693407ebfee9d63bc431194dfc43fd9d3217758e
mwendamwenda <benmwenda83@gmail.com>
2023-01-12 01:48:27 -0800

    logical

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log('its a new begining')
+const hasDriversLicense = true;//A
+const hasGoodVision = true;//B
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(hasDriversLicense);
+
+const shouldDrive = hasDriversLicense && hasGoodVision;
+
+// if(hasDriversLicense && hasGoodVision){
+//     console.log('sarah is able to drive');
+// }else{
+//     console.log('someone should drive');
+// }
+
+const isTired = false;
+console.log(hasDriversLicense || hasGoodVision || isTired);
+console.log(hasDriversLicense && hasGoodVision && isTired);
+
+
+if(hasDriversLicense && hasGoodVision && !isTired){
+    console.log('sarah is able to drive');
+}else{
+    console.log('someone should drive');
+}
