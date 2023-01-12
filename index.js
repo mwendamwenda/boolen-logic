a4a2b84c2385e72dee0e0c66b3d1224d5e0e0e42
mwendamwenda <benmwenda83@gmail.com>
2023-01-12 02:55:29 -0800

    last commit

--- index.js
+++ index.js
@@ -22,3 +22,19 @@ if(hasDriversLicense && hasGoodVision && !isTired){
 }else{
     console.log('someone should drive');
 }
+
+//code challenge
+
+const dolphinAverage = ((97+112+101)/3);
+const koalasAverage = ((109+95+106)/3);
+console.log(dolphinAverage,koalasAverage);
+
+if(dolphinAverage > koalasAverage && dolphinAverage >= 100){
+ console.log('dolphins wins the trophy');
+}else if(dolphinAverage === koalasAverage && dolphinAverage >= 100 && koalasAverage >= 100){
+    console.log('both wins the trophy')
+}else if(koalasAverage > dolphinAverage && koalasAverage >= 100){
+    console.log('koalas wins the trophy');
+}else{
+    console.log('no one wins the trophy');
+}
